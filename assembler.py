d70f72d41c0d4ff2deaadd1dbb83e6bafd32d040
Pavlov Nikita <nikniknik388@gmail.com>
2024-11-20 23:24:21 +0300

    Update assembler.py
    Create class Assembler

--- assembler.py
+++ assembler.py
@@ -2,6 +2,12 @@ import argparse
 import xml.etree.ElementTree as ET
 import xml.dom.minidom
 
+class Assembler:
+    def __init__(self, path_to_code, path_to_binary_file, path_to_log):
+        self.binary_file_path = path_to_binary_file
+        self.code_path = path_to_code
+        self.log_path = path_to_log
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input", help="Входной файл (.asm)")
