90f2bd5f7a056627619dcd47bb22799da241241a
Pavlov Nikita <nikniknik388@gmail.com>
2024-11-20 23:31:12 +0300

    Update assembler.py
    Add commands

--- assembler.py
+++ assembler.py
@@ -11,11 +11,45 @@ class Assembler:
         self.bytes = []
         self.log_root = ET.Element("log")
 
+    def load_constant(self, A, B, C):
+        
+        return bits
+
+    def read_memory(self, A, B, C):
+       
+        return bits
+
+    def write_memory(self, A, B, C):
+        
+        return bits
+
+    def multiply(self, A, B, C, D):
+        
+        return bits
+
     def assemble(self):
         with open(self.code_path, "rt") as code:
             for line in code:
                 line = line.split('\n')[0].strip()
                 if not line: continue
+
+                command, *args = line.split()
+
+                match command:
+                    case "LOAD_CONSTANT":                    
+                        self.bytes.append(self.load_constant(int(args[0]), int(args[1]), int(args[2])))
+
+                    case "READ_MEMORY":
+                        self.bytes.append(self.read_memory(int(args[0]), int(args[1]), int(args[2])))
+
+                    case "WRITE_MEMORY":                        
+                        self.bytes.append(self.write_memory(int(args[0]), int(args[1]), int(args[2])))
+
+                    case "MUL":
+                        self.bytes.append(self.multiply(int(args[0]), int(args[1]), int(args[2]), int(args[3])))
+
+                    case _:
+                        raise SyntaxError(f"{line}\nНеизвестная команда")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
